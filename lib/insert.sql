3530656d01087492cecfb81bcbcc8772a0669f19
Marcus Jun <mjun@calstate.edu>
2018-08-21 23:31:23 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,3 +1,5 @@
 INSERT INTO series (title, author_id, subgenre_id) VALUES ("Chronicles of", 1, 1), ("Ancient tomes",2,2);
 
 INSERT INTO books (title, year, series_id) VALUES ("Shadowlands", 1999, 1), ("Tundralands", 2000, 1), ("Cloudlands", 2001, 1);
+
+INSERT INTO books (title, year, series_id) VALUES ("Begin-inings", 2007, 2), ("Middling-ings", 2008, 2), ("Ending-ings", 2009, 2);
