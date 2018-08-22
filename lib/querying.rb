08775c30c76080e0c5e23452be54ca5c73685f33
Marcus Jun <mjun@calstate.edu>
2018-08-22 01:13:45 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -29,11 +29,11 @@ def select_series_title_with_most_human_characters
 end
 
 def select_character_names_and_number_of_books_they_are_in
-  "SELECT characters.name, COUNT(characters.name)
+  "SELECT characters.name
   FROM characters, books
   INNER JOIN character_books
   ON characters.id = character_books.character_id AND books.id = character_books.book_id
-
+  GROUP BY , COUNT(characters.name)
 
   ;"
 end
