91e62ae2cc18d27240c7e27196bdced6e72d36b6
Marcus Jun <mjun@calstate.edu>
2018-08-22 01:05:52 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -22,8 +22,8 @@ def select_series_title_with_most_human_characters
   FROM series
   INNER JOIN characters
   ON series.id = characters.series_id
-  GROUP BY characters.species 
-  ORDER BY COUNT(characters.species) 
+  GROUP BY characters.species
+  ORDER BY COUNT(characters.species)
   DESC LIMIT 1;"
 end
 
