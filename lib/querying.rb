545633d2d209fd016b56437edbded98b3ae73e71
Marcus Jun <mjun@calstate.edu>
2018-08-22 00:49:56 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -11,8 +11,8 @@ def select_value_and_count_of_most_prolific_species
 end
 
 def select_name_and_series_subgenres_of_authors
-  "SELECT authors.name, series.subgenre_id
-  FROM authors
+  "SELECT authors.name, subgenres.name
+  FROM authors, subgenres
   INNER JOIN series
   ON authors.id = series.author_id;"
 end
