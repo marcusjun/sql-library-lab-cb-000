347a8aca7ae26edc195e50ec6cd1f6522d6d6a86
Marcus Jun <mjun@calstate.edu>
2018-08-22 00:52:38 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -12,9 +12,9 @@ end
 
 def select_name_and_series_subgenres_of_authors
   "SELECT authors.name, subgenres.name
-  FROM authors
+  FROM authors, subgengres
   INNER JOIN series
-  ON authors.id = series.author_id;"
+  ON authors.id = series.author_id, subgenres.id = series.subgenre_id;"
 end
 
 def select_series_title_with_most_human_characters
