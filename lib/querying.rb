13ca4e6ee44e54b8fffed5951a36a143821cfad2
Marcus Jun <mjun@calstate.edu>
2018-08-22 01:14:23 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -33,7 +33,7 @@ def select_character_names_and_number_of_books_they_are_in
   FROM characters, books
   INNER JOIN character_books
   ON characters.id = character_books.character_id AND books.id = character_books.book_id
-  ORDER BY COUNT(characters.name) DESC
+  GROUP BY COUNT(characters.name) DESC
 
   ;"
 end
