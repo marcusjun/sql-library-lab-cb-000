d9c41313aa16ab96b0fbe58fe5bddcc236d826d6
Marcus Jun <mjun@calstate.edu>
2018-08-22 00:27:47 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -6,9 +6,8 @@ def select_name_and_motto_of_char_with_longest_motto
   "SELECT characters.name, characters.motto FROM characters ORDER BY LENGTH(characters.motto) DESC LIMIT 1;"
 end
 
-
 def select_value_and_count_of_most_prolific_species
-  "Write your SQL query here"
+  "SELECT characters.species, COUNT(characters.species) FROM characters ORDER BY COUNT(characters.species) DESC LIMIT 1;"
 end
 
 def select_name_and_series_subgenres_of_authors
