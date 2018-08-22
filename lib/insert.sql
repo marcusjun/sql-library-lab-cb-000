0660cdebcb8b47f31632c821f2165467f8c4241f
Marcus Jun <mjun@calstate.edu>
2018-08-22 00:01:29 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -9,4 +9,4 @@ INSERT INTO authors (name) VALUES ("Clawton Drumbeast"), ("Edwina the Oracle");
 
 INSERT INTO characters (name, species, motto, series_id, author_id) VALUES ("Support Feedback", "warrior", "I'm not here to make friends.", 1, 1), ("General Case", "magician", "Pick a card, any card.", 1, 1), ("In Advance", "healer", "Who needs a bandage?", 1, 1), ("Or Priority", "archer", "Bow and arrow.", 1, 1),
 
-
+("Show The", "rogue", "Backstab or frontstab.", 2, 2), ("First Six", "oragle", "I see the future.", 2, 2), ("First Remark", "driver", "The cavalry is here.", 2, 2), ("Sense Of", "tactician", "Strategy wins battles", 2, 2);
